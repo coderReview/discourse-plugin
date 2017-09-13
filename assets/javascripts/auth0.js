6c93cd0278e2395fdccf960478d86627872599f1
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2017-09-13 17:45:17 -0300

    My Test

--- assets/javascripts/auth0.js
+++ assets/javascripts/auth0.js
@@ -30,7 +30,7 @@
         domain: domain,
         clientID: client_id,
         scope: 'openid email profile',
-        responseType: 'code token id_token',
+        responseType: 'code',
         audience: Discourse.SiteSettings.auth0_audience,
         redirectUri: Discourse.SiteSettings.auth0_callback_url
       });
